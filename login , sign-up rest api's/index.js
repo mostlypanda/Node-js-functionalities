d2e2c3ed01ad4b0764924ed694616ee473c1ead4
mostlypanda <sarthakmittal1461@gmail.com>
2020-06-20 13:42:06 +0530

    database connected

--- login , sign-up rest api's/index.js
+++ login , sign-up rest api's/index.js
@@ -1,13 +1,28 @@
 const express=require('express');
 const mongooses= require('mongoose');
 const User=require('./models/user');
+const db=require('./mysetup/myurl').myurl;
+const bodyparser=require('body-parser');
 
 var app=express();
+// app use
+app.use(bodyparser.urlencoded({extended : false}));
+app.use(bodyparser.json());
+
+// database connection
+mongooses.connect(db).then(()=>{
+    console.log('databse is connected');
+}).catch(err=>{
+    console.log("err is", err.message);
+})
+
+
 
 app.get('/',function(req,res){
     res.status(200).send(`hyy`);
 })
 
+// listening port
 const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>{
     console.log(`app is live at ${PORT}`);
---
+++ login , sign-up rest api's/mysetup/myurl.js
@@ -0,0 +1,4 @@
+module.exports = {
+    myurl: 'mongodb://localhost:27017/Users',
+    secret: 'mysecretkey'
+ }
