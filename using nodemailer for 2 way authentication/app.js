a205a2d16eeda920cfd7f25b7708fd6f8ebc96a1
mostlypanda <sarthakmittal1461@gmail.com>
2020-06-22 20:05:02 +0530

    opt verification done

--- using nodemailer for 2 way authentication/app.js
+++ using nodemailer for 2 way authentication/app.js
@@ -23,6 +23,10 @@ app.get('/',function(req,res){
     res.render('contact');
 });
 
+var otp = Math.random();
+otp = otp * 1000000;
+otp = parseInt(otp);
+console.log(otp);
 
 app.post('/send',function(req,res){
     const output= `
@@ -34,9 +38,50 @@ app.post('/send',function(req,res){
     <li> email: ${req.body.email}</li>
     <li> phone: ${req.body.phone}</li>
     </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
 
     `;
-});
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        service : 'Gmail',
+        
+        auth: {
+          user: 'sarthakmittal1461@gmail.com',
+          pass: 'S@rth@k09',
+        }
+        
+      });
+        
+      // send mail with defined transport object
+      var mailOptions={
+         to: req.body.email,
+        subject: "Otp for registration is: ",
+        html: "<h3>OTP for account verification is </h3>"  + "<h1 style='font-weight:bold;'>" + otp +"</h1>" // html body
+      };
+      
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);   
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  
+        res.render('otp');
+    });
+    });
+
+app.post('/sendotp',function(req,res){
+    if(req.body.otp==otp){
+        res.send("welcome");
+    }
+    else{
+        res.render('otp',{msg : 'otp is incorrect'});
+    }
+})    
 
 const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>{
