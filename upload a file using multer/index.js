0c0434192824a841ab6dd037dc5609dd8fe4b3ae
mostlypanda <sarthakmittal1461@gmail.com>
2020-06-28 13:30:24 +0530

    many changes

--- upload a file using multer/index.js
+++ upload a file using multer/index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const bodyparser=require('body-parser');
 const path=require('path');
 const multer=require('multer');
-const exphbs=require('express-handlebars');
+const ejs=require('ejs');
 
 //defining storage
 var storage=multer.diskStorage({
@@ -12,14 +12,15 @@ var storage=multer.diskStorage({
     filename: function(req,file,cb){
         cb(null,file.filename+ '-'+ Date.now());
     }
-})
+});
+
+var upload=multer({storage : storage}).single('userPhoto');
 
 
 const app=express();
 
 //set engine
-app.engine('handlebars',exphbs({ extname: "hbs", defaultLayout: false, layoutsDir: "views/ "}));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 
 //using json
 app.use(bodyparser.json());
@@ -30,8 +31,17 @@ app.use('/public',express.static(path.join(__dirname + 'public')));
 
 
 app.get('/',function(req,res){
-    res.status(400).json({msg: "hyyy"});
+    res.send('hy');
 })
+
+app.post('/api/upload',function(req,res){
+    upload(req,res,function(err){
+        if(err){
+            res.status(400).end('error');
+        }
+        res.status(200).end('file uploaded');
+    })
+});
 
 //port is live
 const port=3000|| process.env.PORT;
