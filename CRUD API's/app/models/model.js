9163862d5bd0e113ccb5167a9475a46587927169
sarthak mittal <sarthakmittal1461@gmail.com>
2020-07-06 17:18:18 +0530

    Update model.js

--- CRUD API's/app/models/model.js
+++ CRUD API's/app/models/model.js
@@ -4,15 +4,12 @@ const mongoose=require('mongoose');
 const noteSchema=mongoose.Schema({
     title:{
         type: String,
-        require: true
     },
     author:{
         type: String,
-        require: true
     },
     content:{
         type : String,
-        require: true
     }
 },{
     timestamps: true
