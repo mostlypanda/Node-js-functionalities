8d32647c3a253a1640ed03ceaaf99c05230853cd
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-08 19:21:45 +0530

    schema created

---
+++ payment gateway/models/model.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+
+const donorschema=mongoose.Schema({
+    name:{
+        type : String,
+        required : true
+    },
+    email:{
+        type : String,
+        required : true
+    },
+    amount:{
+        type :String,
+        required : true
+    },
+    reference:{
+        type : String,
+        required : true
+    }
+});
+
+module.exports=mongoose.model('donor',donorschema);
