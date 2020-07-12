585efffd7e820120f22870061326f0ed58827bcd
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-12 16:56:32 +0530

    some changes

--- restful api for file uploading/api.js
+++ restful api for file uploading/api.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const bodyparser=require('body-parser');
 const mongoose=require('mongoose');
+const multer=require('multer');
 const user=require('./model');
 const app=express();
 
