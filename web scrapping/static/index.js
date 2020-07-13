e2555882908460bd43c5a938a28b4800a7299ab8
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-13 23:35:13 +0530

    static content done

--- web scrapping/static/index.js
+++ web scrapping/static/index.js
@@ -5,11 +5,24 @@ const app=express();
 
 const url="http://news.ycombinator.com";
 
+let getData = html => {
+    data = [];
+    const $ = cheerio.load(html);
+    $('table.itemlist tr td:nth-child(3)').each((i, elem) => {
+      data.push({
+        title : $(elem).text(),
+        link : $(elem).find('a.storylink').attr('href')
+      });
+    });
+    return data;
+  }
+
 app.get('/',function(req,res){
     axios.get(url)
     .then(resp=>{
-        console.log(resp.data);
-        res.send(resp.data);
+        let html=resp.data;
+        console.log(getData(html));
+        res.send(getData(html));
     }).catch(err=>{
         console.log(err);
     });
