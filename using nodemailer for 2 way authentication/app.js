eb54c9c1b9dc61ea6cb67707ef3edcd034c6d5ff
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-17 00:17:56 +0530

    new routes has been added

--- using nodemailer for 2 way authentication/app.js
+++ using nodemailer for 2 way authentication/app.js
@@ -23,6 +23,8 @@ app.get('/',function(req,res){
     res.render('contact');
 });
 
+var email;
+
 var otp = Math.random();
 otp = otp * 1000000;
 otp = parseInt(otp);
@@ -39,20 +41,18 @@ let transporter = nodemailer.createTransport({
       pass: 'S@rth@k09',
     }
     
-  });
+});
     
- 
-
-
 app.post('/send',function(req,res){
-    
+    email=req.body.email;
+
      // send mail with defined transport object
     var mailOptions={
         to: req.body.email,
        subject: "Otp for registration is: ",
        html: "<h3>OTP for account verification is </h3>"  + "<h1 style='font-weight:bold;'>" + otp +"</h1>" // html body
      };
-
+     
      transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             return console.log(error);
@@ -64,15 +64,33 @@ app.post('/send',function(req,res){
     });
 });
 
-app.post('/sendotp',function(req,res){
+app.post('/verify',function(req,res){
 
     if(req.body.otp==otp){
-        res.send("welcome");
+        res.send("You has been successfully registered");
     }
     else{
         res.render('otp',{msg : 'otp is incorrect'});
     }
-});   
+});  
+
+app.post('/resend',function(req,res){
+    var mailOptions={
+        to: email,
+       subject: "Otp for registration is: ",
+       html: "<h3>OTP for account verification is </h3>"  + "<h1 style='font-weight:bold;'>" + otp +"</h1>" // html body
+     };
+     
+     transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);   
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        res.render('otp',{msg:"otp has been sent"});
+    });
+
+});
 
 const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>{
